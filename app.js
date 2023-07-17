97d0ce16828e71ada8869b99ae7cba2e8c2c62e2
Raffaele <devout.official@gmail.com>
2023-07-17 16:46:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var buttonPlay = document.getElementById("button-play");
+var numberRandom = Math.floor(Math.random() * 101);
+buttonPlay.addEventListener("click", "numberRandom");
---
+++ app.ts
@@ -0,0 +1,4 @@
+let buttonPlay:any = document.getElementById("button-play")
+let numberRandom:any = Math.floor(Math.random()*101);
+
+buttonPlay.addEventListener("click", "numberRandom");
